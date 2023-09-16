6a3ba4fe223b8555cb97bea1963fc451c6478ae0
xurshidbek <shamsun.com@gmail.com>
2023-09-16 15:58:14 +0500

    BB commit

--- src/main/java/com/example/projectboard/ProjectBoardApplication.java
+++ src/main/java/com/example/projectboard/ProjectBoardApplication.java
@@ -8,7 +8,7 @@ public class ProjectBoardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectBoardApplication.class, args);
-        System.out.println("Spring Boot project AA");
+        System.out.println("Spring Boot project BB");
     }
 
 }
